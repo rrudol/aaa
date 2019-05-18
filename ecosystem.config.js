0101e83c86fea464f9c16bc3d52d71916bf3b4b7
Rafał Rudol <rrrrudol@gmail.com>
2019-05-18 16:21:22 +0200

    add another host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,33 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'app.js',
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: "API",
+      script: "app.js",
+      args: "one two",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
-  deploy : {
-    production : {
-      user : 'ubuntu',
-      host : 'ec2-3-120-98-206.eu-central-1.compute.amazonaws.com',
-      ref  : 'origin/master',
-      repo : 'https://github.com/rrudol/aaa.git',
-      path : '/tmp/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  ],
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: [
+        "ec2-3-120-98-206.eu-central-1.compute.amazonaws.com",
+        "ec2-18-185-18-173.eu-central-1.compute.amazonaws.com"
+      ],
+      ref: "origin/master",
+      repo: "https://github.com/rrudol/aaa.git",
+      path: "/tmp/www/production",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
